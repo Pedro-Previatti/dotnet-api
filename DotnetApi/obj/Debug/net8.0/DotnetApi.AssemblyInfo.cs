a3393939995c14048b87d0b92f71ac949048d162
Pedro Previatti <pvlpreviatti@gmail.com>
2024-06-10 20:46:18 -0300

    a

--- DotnetApi/obj/Debug/net8.0/DotnetApi.AssemblyInfo.cs
+++ DotnetApi/obj/Debug/net8.0/DotnetApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotnetApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe9062542b59c47efb50b2c988f2ea0b22b9b4b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb25bafaeda0e151621f37abc01db827a46c4b9e")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotnetApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotnetApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
