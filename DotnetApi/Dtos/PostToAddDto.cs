880fdd62818d2fdb0c7a404e743d209754d142fb
Pedro Previatti <pvlpreviatti@gmail.com>
2024-06-20 16:42:44 -0300

    (feat): created 2 new Dtos, PostToAdd and PostToEdit

---
+++ DotnetApi/Dtos/PostToAddDto.cs
@@ -0,0 +1,14 @@
+namespace DotnetApi.Models
+{
+  public partial class PostToAddDto
+  {
+    public string PostTitle { get; set; }
+    public string PostContent { get; set; }
+
+    public PostToAddDto()
+    {
+      PostTitle ??= "";
+      PostContent ??= "";
+    }
+  }
+}
